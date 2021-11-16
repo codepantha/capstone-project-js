cefd97b3087c2e1f08772dc7b8a62eceb79f3462
Carlos-SSH <carlos.anezeh@me.com>
2021-11-16 15:48:38 -0600

    Add style to the modal container and align to the center a button.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,10 @@ import './style.css';
 import nav from './components/nav';
 import pop from './components/pop';
 
-
 const btnPop = () => {
   const btn = document.createElement('button');
   btn.innerHTML = 'COMMENTS';
+  btn.id = 'open';
   btn.classList.add('btn');
 
   btn.addEventListener('click', () => {
--- src/components/pop.js
+++ src/components/pop.js
@@ -1,7 +1,33 @@
 const pop = () => {
-  const popup = document.createElement('div');
-  popup.id = 'pop';
-  popup.classList.add('show');
+  const body = document.body;
+  const container = document.createElement('div');
+  const modal = document.createElement('div');
+  const h1 = document.createElement('h1');
+  const p = document.createElement('p');
+  const button = document.createElement('button');
+
+  container.classList.add('modal-container');
+  container.id = 'modal-container';
+
+  modal.classList.add('modal');
+  modal.id = 'modal';
+
+  h1.classList.add('modal-title');
+  h1.innerText = 'Comments Section';
+
+  p.innerText = 'lorem ipsum dolor sit amet, consectetur adipiscing';
+
+  button.innerText = 'Close';
+  button.id = 'close-btn';
+  button.classList.add('modal-close');
+  button.classList.add('btn');
+
+  body.appendChild(container);
+  container.appendChild(modal);
+  modal.appendChild(h1);
+  modal.appendChild(p);
+  modal.appendChild(button);
+
 }
 
 export default pop;
