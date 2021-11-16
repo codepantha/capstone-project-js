208470af078beba58928749d5d60a4096cca17d1
Carlos-SSH <carlos.anezeh@me.com>
2021-11-16 14:26:41 -0600

    Uncommented code.

--- src/app.js
+++ src/app.js
@@ -1,22 +1,22 @@
 import './style.css';
 import nav from './components/nav';
 
-// const pop = () => {
-//   const popup = document.createElement('div');
-//   popup.id = 'pop';
-//   popup.classList.add('show');
-// }
+const pop = () => {
+  const popup = document.createElement('div');
+  popup.id = 'pop';
+  popup.classList.add('show');
+}
 
-// const btnPop = () => {
-//   const btn = document.createElement('button');
-//   btn.innerHTML = 'COMMENTS';
-//   btn.classList.add('btn');
+const btnPop = () => {
+  const btn = document.createElement('button');
+  btn.innerHTML = 'COMMENTS';
+  btn.classList.add('btn');
 
-//   btn.addEventListener('click', () => {
-//     pop();
-//   });
-//   document.body.appendChild(btn);
-// }
+  btn.addEventListener('click', () => {
+    pop();
+  });
+  document.body.appendChild(btn);
+}
 
 const startApp = () => {
   nav();
