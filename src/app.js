a7491491c7c1520d420f6ef1ca2a42d2dbece2bd
Carlos-SSH <carlos.anezeh@me.com>
2021-11-16 11:31:48 -0600

    Add a image for navbar.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,27 @@
 import './style.css';
+import Logo from './assets/images/hp-logo.png';
+
+const nav = () => {
+  const header = document.getElementById('header');
+  const nav = document.createElement('nav');
+  nav.innerHTML = `
+    <a href="#" class="logo">
+      <img src='${Logo}' alt="Logo Harry">
+    </a>
+    <ul>
+      <li>
+        <a href="#">Home</a>
+      </li>
+      <li>
+        <a href="#">About</a>
+      </li>
+    </ul>
+  `;
+  header.appendChild(nav);
+};
+
+const startApp = () => {
+  nav();
+}
+
+startApp();
