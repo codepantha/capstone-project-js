9ac7e779d7b752c689b43ab9d2d56cb11e4dc666
Carlos-SSH <carlos.anezeh@me.com>
2021-11-16 12:40:45 -0600

    Create a components directory where we create a new file that contains the navbar that its imported into app.js.

--- src/app.js
+++ src/app.js
@@ -1,24 +1,5 @@
 import './style.css';
-import Logo from './assets/images/hp-logo.png';
-
-const nav = () => {
-  const header = document.getElementById('header');
-  const nav = document.createElement('nav');
-  nav.innerHTML = `
-    <a href="#" class="logo">
-      <img class="img" src='${Logo}' alt="Logo Harry">
-    </a>
-    <ul>
-      <li>
-        <a class="link" href="#">About</a>
-      </li>
-      <li>
-        <a class="link" href="#">Home</a>
-      </li>
-    </ul>
-  `;
-  header.appendChild(nav);
-};
+import nav from './components/nav';
 
 const startApp = () => {
   nav();
