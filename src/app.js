660a21571924bb38cbb7f672136d81af415bd279
Carlos-SSH <carlos.anezeh@me.com>
2021-11-16 13:37:34 -0600

    Add linters.yml.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,29 @@
 import './style.css';
 import nav from './components/nav';
 
+
+// const pop = () => {
+//   const popup = document.createElement('div');
+//   popup.id = 'pop';
+//   popup.classList.add('show');
+// }
+
+
+// const btnPop = () => {
+//   const btn = document.createElement('button');
+//   btn.innerHTML = 'COMMENTS';
+//   btn.classList.add('btn');
+
+//   btn.addEventListener('click', () => {
+//     pop();
+//   });
+//   document.body.appendChild(btn);
+// }
+
 const startApp = () => {
   nav();
+  // btnPop();
+  // pop();
 }
 
 startApp();
