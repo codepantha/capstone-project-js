c8080014a42d3677c955c99d7bdb782bc0b449e4
Carlos-SSH <carlos.anezeh@me.com>
2021-11-17 13:46:08 -0600

    Display data from api on dom.

--- src/components/pop.js
+++ src/components/pop.js
@@ -5,49 +5,59 @@ const pop = () => {
   const body = document.body;
   const container = document.createElement('div');
   const modal = document.createElement('div');
+  const infoContainer = document.createElement('div');
+  const descriptionContainer = document.createElement('div');
   const commentsContainer = document.createElement('div');
   const formContainer = document.createElement('div');
   const h1 = document.createElement('h1');
-  const p = document.createElement('p');
+  const p1 = document.createElement('p');
+  const p2 = document.createElement('p');
+  const p3 = document.createElement('p');
   const button = document.createElement('button');
   const btn = document.createElement('button');
   const img = document.createElement('img');
   const headComments = document.createElement('h2');
   const comments = document.createElement('p');
   const response = getCharacter();
-
+  
+  
   response
-    .then(res => {
-      const Name = res[0].name;
-      const Actor = res[0].actor;
-      const House = res[0].house;
-      const Species = res[0].species;
-      const Image = res[0].image;
+  .then(res => {
+      h1.innerHTML = res[0].name;
+      h1.id = 'modal-title';
+      
+      p1.innerHTML= `<strong>Actor: </strong> <span>${res[0].actor}</span>`;
+      p1.classList.add = 'modal-actor';
+      p1.id = 'modal-content';
 
-      console.log(Name);
-      console.log(Actor);
-      console.log(House);
-      console.log(Species);
-      console.log(Image);
+      p2.innerHTML= `<strong>House: </strong> <span>${res[0].house}</span>`;
+
+      p2.id = 'modal-content';
+
+      p3.innerHTML= `<strong>Specie: </strong> <span>${res[0].species}</span>`;
+      p3.id = 'modal-content';
+
+      img.src = res[0].image;
+      img.classList.add = 'modal-img';
+      img.id = 'modal-img';
     })
     .catch(err => {
       console.log(err);
     });
   
-  // h1.innerHTML = objName;
-
   container.classList.add('modal-container');
   container.id = 'modal-container';
 
   modal.classList.add('modal');
   modal.id = 'modal';
 
-  // p.innerText = info.description;
-  p.classList.add('modal-content');
+  p1.classList.add('modal-content');
 
   img.src = defaultIMG;
   img.classList.add('modal-img');
   img.alt = 'Harry Potter Side Image';
+
+  descriptionContainer.classList.add('description-container');
 
   commentsContainer.classList.add('comments-container');
   commentsContainer.id = 'comments-container';
@@ -73,10 +83,14 @@ const pop = () => {
   document.body.appendChild(btn);
   body.appendChild(container);
   container.appendChild(modal);
-  // modal.appendChild();
-  modal.appendChild(h1);
-  modal.appendChild(img);
-  modal.appendChild(p);
+  // modal.appendChild(close);
+  modal.appendChild(infoContainer);
+  infoContainer.appendChild(h1);
+  infoContainer.appendChild(img);
+  infoContainer.appendChild(descriptionContainer);
+  descriptionContainer.appendChild(p1);
+  descriptionContainer.appendChild(p2);
+  descriptionContainer.appendChild(p3);
   modal.appendChild(commentsContainer);
   modal.appendChild(formContainer);
   
