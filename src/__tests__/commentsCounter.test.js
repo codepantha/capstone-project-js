c0c6636005410898d72eb3ab0f1efda43cf51f7a
codepanther <pezechigozirim@gmail.com>
2021-11-19 20:50:17 +0100

    Fix linter issue

--- src/__tests__/commentsCounter.test.js
+++ src/__tests__/commentsCounter.test.js
@@ -1,9 +1,9 @@
-import commentsCounter from "../commentsCounter";
+import commentsCounter from '../commentsCounter';
 
-jest.mock("../commentsCounter");
+jest.mock('../commentsCounter');
 
 test('assert that the number of comments is 4', () => {
   const comments = ['comment1', 'comment2', 'comment3', 'comment4'];
 
   expect(commentsCounter((comments))).toBe(comments.length);
-})
+});
